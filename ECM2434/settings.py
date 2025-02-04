627ca70f4404f611e3d39943cacd8f284af42ffd
E1t1h1a1n1 <ethan1sweeney2@gmail.com>
2025-02-04 10:35:08 +0000

    moved README.md
    Signed-off-by: E1t1h1a1n1 <ethan1sweeney2@gmail.com>

--- ECM2434/settings.py
+++ ECM2434/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Accounts",
+    "EcoWorld",
 ]
 
 MIDDLEWARE = [
