ecc49352ffe1ff4d6a7c94d982712778d2072cc7
lewisFarley403 <lewis.farley1@btinternet.com>
2025-02-05 01:52:34 +0000

    added card models

--- EcoWorld/models.py
+++ EcoWorld/models.py
@@ -16,6 +16,9 @@ class ongoingChallenge(models.Model):
     submitted_on = models.DateField()
     def __str__(self):
         return self.challenge.name + " by " + self.user.username
+    
+
+    
 class waterFountain(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
@@ -27,3 +30,22 @@ class drinkEvent(models.Model):
     drank_on = models.DateField()
     def __str__(self):
         return self.user.username + " drank from " + self.fountain.name
+    
+
+
+class cardRarity(models.Model):
+    title = models.CharField(max_length=50)
+    def __str__(self):
+        return self.title
+class card(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    rarity = models.ForeignKey(cardRarity, on_delete=models.CASCADE)
+    image = models.ImageField()
+    def __str__(self):
+        return self.title
+class ownsCard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card = models.ForeignKey(card, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username + " owns " + self.card.title
