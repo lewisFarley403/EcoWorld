62c335fb2eebaf133b710ae5134a57871a085924
E1t1h1a1n1 <ethan1sweeney2@gmail.com>
2025-02-06 15:30:53 +0000

    migrations

--- Accounts/migrations/0003_profile_first_name_profile_last_name.py
+++ Accounts/migrations/0003_profile_first_name_profile_last_name.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("accounts", "0002_alter_profile_profile_picture"),
+        ("Accounts", "0002_alter_profile_profile_picture"),
     ]
 
     operations = [
