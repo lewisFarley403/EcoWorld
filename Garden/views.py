04c06c213d41b74cf8b480d7e0041afec1b30dca
lewisFarley403 <lewis.farley1@btinternet.com>
2025-02-08 01:31:06 +0000

    waiting on the new db for quant of cards in ownership

--- Garden/views.py
+++ Garden/views.py
@@ -10,13 +10,18 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 @login_required  # Ensure that only logged-in users can access the profile
 def show_garden(request):
-    print(type(request.user))
     g = garden.objects.get(userID=request.user)
     squares = g.gardensquare_set.all()
-    print(list(squares))
     processedSquares = [[squares[i*g.size+j] for j in range (g.size)] for i in range (g.size)]
-    print(processedSquares)
-    return render(request, 'Garden/garden.html', {'squares': processedSquares,'MEDIA_URL':settings.MEDIA_URL,'size':g.size})
+
+
+    playerInventory = ownsCard.objects.filter(user=request.user)
+    availableCards = [ card.card for card in playerInventory if card.card not in [square.cardID for square in squares]]
+    serialized=json.loads(serializers.serialize('json', availableCards))
+    print("SERIALIZED")
+    final = [obj["fields"] for obj in serialized]
+    print(final[0])
+    return render(request, 'Garden/garden.html', {'squares': processedSquares,'MEDIA_URL':settings.MEDIA_URL,'size':g.size,'availableCards':final})
 def remove_card(request):
     if request.method =="POST":
         print("REQUEST IS POST")
@@ -40,11 +45,6 @@ def getAvailableCards(request):
     squares = g.gardensquare_set.all()
     playerInventory = ownsCard.objects.filter(user=request.user)
     availableCards = [ card.card for card in playerInventory if card.card not in [square.cardID for square in squares]]
-    # print(dir(availableCards))
-    # availableCards = []
-    # for square in squares:
-    #     if square.cardID is None:
-    #         availableCards.append(square.squareID)
     serialized=json.loads(serializers.serialize('json', availableCards))
     print("SERIALIZED")
     final = [obj["fields"] for obj in serialized]
