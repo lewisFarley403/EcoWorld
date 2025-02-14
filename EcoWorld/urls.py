03358b3a497cb02f22536dbaf9eedaa525bbf72c
Theo Armes <theo.armes@gmail.com>
2025-02-14 12:09:57 +0000

    i think fixed

--- EcoWorld/urls.py
+++ EcoWorld/urls.py
@@ -6,7 +6,6 @@ app_name = 'EcoWorld'
 urlpatterns = [
     path('addDrink/', addDrink, name='home'),  # Root URL
     path("drink/", testAddDrink, name='addDrinkTest'),  # URL for the signup page
-    path('ecoworld/', include('EcoWorld.urls', namespace='EcoWorld')),
     path('scan/', scan_qr_code, name='scan_qr'),
     path('upload_photo/', upload_bottle_photo, name="upload_photo"),
 
