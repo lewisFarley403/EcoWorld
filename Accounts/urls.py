d34c17436b89c3c28919a3e49249c76c3dc1e72d
E1t1h1a1n1 <ethan1sweeney2@gmail.com>
2025-03-04 17:42:50 +0000

    Forgot password button and functionality added, just need to figure out storage of details for email (UN and PW)

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -18,6 +18,10 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('api/userinfo/', views.user_info, name='user_info'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     
 ]
