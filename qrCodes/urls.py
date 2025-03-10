5765ca5320d1605baa005f2c50fa9fc684966796
E1t1h1a1n1 <ethan1sweeney2@gmail.com>
2025-03-10 10:42:19 +0000

    renaming of files for readability, changing of urls to match new names. Cleaned up imports and made it so only admins can access the generate_qr_cde view.

--- qrCodes/urls.py
+++ qrCodes/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import makeQrCode, scanCode,scanCodePage
+from .views import generate_qr_code, scan_code, scan_code_page
 
 urlpatterns = [
-    path('makeQrCode/', makeQrCode, name='generate_qr'),
-    path('scanCode/', scanCode, name='scan_qr'),
-    path('scanner/', scanCodePage, name='scan_qr_page'),
+    path('generate_qr_code/', generate_qr_code, name='generate_qr'),
+    path('scan_code/', scan_code, name='scan_qr'),
+    path('scanner/', scan_code_page, name='scan_qr_page'),
 ]
--- qrCodes/views.py
+++ qrCodes/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render
 from EcoWorld.models import waterFountain,drinkEvent
-from django.contrib.auth.decorators import login_required
-from django.conf import settings
+from django.contrib.auth.decorators import login_required, permission_required
 from django.utils import timezone
-from datetime import timedelta
 from django.conf import settings
 # Create your views here.
-def makeQrCode(request):
-    # test user is admin
-    # if request.user.is_staff:
+@permission_required("Accounts.can_view_admin_button")  # Only allowed admins can generate new QR codes
+def generate_qr_code(request):
     fountains = waterFountain.objects.all() 
     print(fountains)
-    return render(request, 'makeQrCode.html', {'fountains': fountains,
+    return render(request, 'make_qr_code.html', {'fountains': fountains,
                                                       'width':settings.QR_CODE_WIDTH,
                                                       'height':settings.QR_CODE_HEIGHT})
 @login_required
-def scanCodePage(request):
-    return render(request, 'qrScanner.html')
+def scan_code_page(request):
+    return render(request, 'qr_scanner.html')
 
 @login_required
-def scanCode(request):
+def scan_code(request):
     user = request.user
     fountain_id = request.GET.get('id')
     last_drink = drinkEvent.objects.filter(user=user).order_by('-drank_on').first()
@@ -29,7 +26,7 @@ def scanCode(request):
         print("last drink not none")
         time_difference = timezone.now() - last_drink.drank_on
         if time_difference < settings.DRINKING_COOLDOWN:
-            return render(request,'sorry.html')
+            return render(request, 'drink_cooldown_page.html')
     # More than 20 minutes have passed
     print("User drank more than 20 minutes ago")
     drinkEvent.objects.create(user=user,fountain=waterFountain.objects.get(id=fountain_id))
@@ -37,5 +34,5 @@ def scanCode(request):
     user.profile.number_of_coins += settings.VALUE_OF_DRINK
     user.profile.save()
     
-    return render(request,'thankyou.html')
+    return render(request, 'drink_registered.html')
         
