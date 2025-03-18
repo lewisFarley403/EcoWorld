73d5e97bb74526ab06dc57b5301727fbf280ebe3
lewisFarley403 <lewis.farley1@btinternet.com>
2025-03-18 00:12:24 +0000

    guessing

--- ECM2434/settings.py
+++ ECM2434/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = "static/"
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
-STATIC_ROOT = BASE_DIR / 'static' #deployment only
+# STATIC_ROOT = BASE_DIR / 'static' #deployment only
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
