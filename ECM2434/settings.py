a5b56357c23afba8702dac66e0b2f3cffaaf7bb3
E1t1h1a1n1 <ethan1sweeney2@gmail.com>
2025-03-22 22:42:25 +0000

    styling fixes

--- ECM2434/settings.py
+++ ECM2434/settings.py
@@ -146,12 +146,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Ensure Django knows where to find static files (only needed for local development)
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
-
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
