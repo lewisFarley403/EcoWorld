dfdc4c805ef283ac2aa30ee892f65e9a5bc13673
lewisFarley403 <lewis.farley1@btinternet.com>
2025-03-22 14:58:02 +0000

    added markdown to guides

--- guides/forms.py
+++ guides/forms.py
@@ -8,7 +8,7 @@ class GuidesForm(forms.Form):
     This class provides the form for the gamekeeper to add a guide
     Attributes:
         title : this attribute is the title of the guide
-        content : this attribute is for the content section of the guide
+        content : this attribute is for the content section of the guide (supports Markdown)
     author:
         Johnny Say (js1687@exeter.ac.uk)
     """
@@ -18,8 +18,9 @@ class GuidesForm(forms.Form):
         widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     content = forms.CharField(
-        label="Content",
-        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4}))
+        label="Content (Markdown supported)",
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 8, 
+                                    'placeholder': 'This field supports Markdown formatting. Examples:\n\n# Heading 1\n## Heading 2\n\n**Bold text** or __bold text__\n*Italic text* or _italic text_\n\n- Bullet point\n1. Numbered list\n\n[Link text](https://example.com)\n\n```\nCode block\n```'}))
 
 class DeleteForm(forms.Form):
     """
